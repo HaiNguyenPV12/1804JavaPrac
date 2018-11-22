0fbc885dd25e4a980b2c6b562ff826bf5a5ab135
Hai Nguyen <39506270+HaiNguyenPV12@users.noreply.github.com>
2018-11-22 16:21:11 +0700

    Revert "Revert "initial commit""
    This reverts commit da9401433dda4109e6c5ca346160e5346e3fd1d3.

---
+++ src/javaprac/Student.java
@@ -0,0 +1,19 @@
+package javaprac;
+
+/**
+ * Student
+ */
+public class Student {
+
+    int id, yob;
+    String sex;
+
+    public Student() {
+    }
+
+    public Student(int id, int yob, String sex) {
+        this.id = id;
+        this.yob = yob;
+        this.sex = sex;
+    }
+}
